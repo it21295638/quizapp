30b49ed5bab022e80dfe50acadb3fc78f89178d9
it21295638 <it21295638@my.sliit.lk>
2025-02-23 23:30:23 +0530

    added decoration

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quizapp/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(gradient: LinearGradient(colors: [])),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
